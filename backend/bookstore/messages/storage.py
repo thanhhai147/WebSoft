2b3d7913a07ea54ce4fa43016f7f1e89dfe1553f
Kory <phamtruc.main@gmail.com>
2024-03-29 08:47:04 +0700

    Add messages for book and storage

---
+++ backend/bookstore/messages/storage.py
@@ -0,0 +1,26 @@
+class StorageMessage():
+    #generation: MSG0xxx
+    MSG0001 = "Thành công."
+    MSG0002 = "Thất bại."
+    MSG0003 = "Lỗi dữ liệu."
+
+    # function New Storage: MSG1xxx
+    MSG1001 = "Tạo kho sách thành công"
+    MSG1002 = "Tạo kho sách thất bại"
+
+    # function New Book in Storage: MSG2xxx
+    MSG2001 = "Vui lòng nhập đầy đủ các ô"
+    MSG2002 = "Không tìm thấy kho sách"
+    MSG2003 = "Không tìm thấy Sách"
+    MSG2004 = "Số lượng sách phải lớn hơn 0"
+    MSG2005 = "Giá sách phải lớn hơn 0"
+    MSG2006 = "Thêm Sách vào kho sách thành công"
+    MSG2007 = "Thêm sách vào kho sách thất bại"
+
+
+
+
+
+
+
+

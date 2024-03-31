9175f0706e315260430d2fe55f1907034f64a10f
Anh <22520084@gm.uit.edu.vn>
2024-03-31 09:16:57 +0700

    push

--- frontend/src/pages/login.page.js
+++ frontend/src/pages/login.page.js
@@ -1,28 +1,28 @@
-import React from "react"
-import LoginAPI from "../api/login.api"
+// LoginPage.js
+
+import React from "react";
+import LoginAPI from "../api/login.api";
 import { Button, Form, Input } from 'antd';
 import TokenUtil from "../helpers/token.utils";
+import "./styles/login.page.css";
 
 
 const LoginPage = ({setToken, setUsername}) => {
-    // const [username, setUsername] = useState("");
-    // const [password, setPassword] = useState("");
-
     const onFinish = async (values) => {
         console.log('Success:', values);
         try {
-            let response = await LoginAPI.handleLogin(values)
+            let response = await LoginAPI.handleLogin(values);
             if(response.status === 200) {
-                console.log(response)
-                let token = response.data.data.token
-                let username = response.data.data.username
-                TokenUtil.saveToken(token)
-                setToken(token)
-                setUsername(username)
-                window.location.replace("/book")
+                console.log(response);
+                let token = response.data.data.token;
+                let username = response.data.data.username;
+                TokenUtil.saveToken(token);
+                setToken(token);
+                setUsername(username);
+                window.location.replace("/book");
             }
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     };
     const onFinishFailed = (errorInfo) => {
@@ -30,62 +30,62 @@ const LoginPage = ({setToken, setUsername}) => {
     };
 
     return (
-        <Form
-            name="basic"
-            labelCol={{
-            span: 8,
-            }}
-            wrapperCol={{
-            span: 16,
-            }}
-            style={{
-            maxWidth: 600,
-            }}
-            initialValues={{
-            remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[
-                    {
-                    required: true,
-                    message: 'Please input your username!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
-
-
-            <Form.Item
+        <div className="login-container">
+            <Form
+                className="login-form"
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
                 wrapperCol={{
-                    offset: 8,
                     span: 16,
                 }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
             >
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item
+                    label="Username"
+                    // name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    // name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
     );
 };
 

6dc860ee2ce5f54ad873b6567a8a346e2990493b
Dino200503 <dotruong576@gmail.com>
2024-03-31 23:11:19 +0700

    add consumer messages and serializers

---
+++ backend/bookstore/serializers/consumer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from ..models.consumer import Consumer
+
+class ConsumerSerializer(serializers.Serializer):
+    Name = serializers.CharField(max_length = 255)
+    Address = serializers.CharField(max_length = 255)
+    Phone = serializers.CharField(max_length = 11)
+    Email = serializers.EmailField(max_Length = 320, allow_null = True, allow_blank = True)
+    Debt = serializers.FloatField()
+    Created = serializers.DateTimeField(read_only=True)

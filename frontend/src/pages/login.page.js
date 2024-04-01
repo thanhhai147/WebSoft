041f2c1701f369e8916713595d14ebac92f2b4c2
thanhhai252004 <thanhhai252004@gmail.com>
2024-04-01 15:55:23 +0700

    Add layout folder

--- frontend/src/pages/login.page.js
+++ frontend/src/pages/login.page.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Form, Input } from 'antd';
 import LoginAPI from  "../api/login.api";
 import TokenUtil from '../helpers/token.utils';

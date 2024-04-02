e75a8ed207becf95944ae24ab6af13184bdbbd35
Trúc Phạm <phamtruc.main@gmail.com>
2024-04-02 08:02:40 +0700

    Change url name to kebab

--- backend/bookstore/urls/book.py
+++ backend/bookstore/urls/book.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ..views.book import AddBookTypeAPIVIew, AddAuthorViewAPI, AddBookViewAPI
     
 urlpatterns = [
-    path('bookType/new', AddBookTypeAPIVIew.as_view(), name='new-booktype'),
+    path('book-type/new', AddBookTypeAPIVIew.as_view(), name='new-booktype'),
     path('author/new', AddAuthorViewAPI.as_view(), name='new-author'),
     path('book/new', AddBookViewAPI.as_view(), name='new-book')
 ]
--- backend/bookstore/urls/storage.py
+++ backend/bookstore/urls/storage.py
@@ -3,5 +3,5 @@ from django.urls import path
 from ..views.storage import AddBookToStorageViewAPI
 
 urlpatterns = [
-    path('book/new', AddBookToStorageViewAPI.as_view(), name='addBookToStorage'),
+    path('book/new', AddBookToStorageViewAPI.as_view(), name='add-book-to-storage'),
 ]

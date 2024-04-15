f626eceb29bb0af552a5643952c033b88c84d88b
Dino200503 <dotruong576@gmail.com>
2024-04-15 23:39:03 +0700

    fix: deleted method Delete, format method Get

--- backend/bookstore/urls/payment.py
+++ backend/bookstore/urls/payment.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from ..views.payment import CreatePaymentAPIView,GetAllPaymentDetailAPIView ,GetPaymentDetailAPIView, DeletePaymentAPIView
+from ..views.payment import CreatePaymentAPIView,GetAllPaymentDetailAPIView ,GetPaymentDetailAPIView
 
 urlpatterns = [
     path('payment/create/', CreatePaymentAPIView.as_view(), name='create_payment'),
     path('payment/', GetAllPaymentDetailAPIView.as_view(), name='get_all_payment_detail'),
-    path('payment/<int:pk>/', GetPaymentDetailAPIView.as_view(), name='get_payment_detail'),
-    path('payment/<int:pk>/delete/', DeletePaymentAPIView.as_view(), name='delete_payment'),
+    path('payment/<int:pk>/', GetPaymentDetailAPIView.as_view(), name='get_payment_detail')
 ]

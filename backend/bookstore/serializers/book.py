96f17322e4b1e503ceacf5b2a22f0996cd156e12
Trúc Phạm <phamtruc.main@gmail.com>
2024-04-18 08:48:30 +0700

    Remove length check and added require and null check

--- backend/bookstore/serializers/book.py
+++ backend/bookstore/serializers/book.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-from django.core.validators import MinValueValidator
 
 class BookTypeSerializer(serializers.Serializer):
-    bookTypeName = serializers.CharField(max_length=255)
+    bookTypeName = serializers.CharField(required=True, allow_null=False)
 
 class AuthorSerializer(serializers.Serializer):
-    authorName = serializers.CharField(max_length=255)
+    authorName = serializers.CharField(required=True, allow_null=False)
 
 class BookSerializer(serializers.Serializer):
-    bookName = serializers.CharField(max_length=255)
-    bookTypeId = serializers.IntegerField()
-    authorId = serializers.IntegerField()
+    bookName = serializers.CharField(required=True, allow_null=False)
+    bookTypeId = serializers.IntegerField(required=True, allow_null=False)
+    authorId = serializers.IntegerField(required=True, allow_null=False)
--- backend/bookstore/serializers/storage.py
+++ backend/bookstore/serializers/storage.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 class StorageSerializer(serializers.Serializer):
     date = serializers.DateField()
 
 class BookStorageSerializer(serializers.Serializer):
     bookId = serializers.IntegerField(required=True, allow_null=False)
-    unitPrice = serializers.IntegerField(validators=[MinValueValidator(0)], required=True, allow_null=False)
-    quantity = serializers.IntegerField(validators=[MinValueValidator(0)], required=True, allow_null=False)
+    unitPrice = serializers.IntegerField(required=True, allow_null=False)
+    quantity = serializers.IntegerField(required=True, allow_null=False)

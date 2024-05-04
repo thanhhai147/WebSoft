14e0c931fea4eeafcc0e939e78993bf445c6ed24
Trúc Phạm <phamtruc.main@gmail.com>
2024-05-04 10:32:28 +0700

    fix: Add get book storage

--- backend/bookstore/urls/storage.py
+++ backend/bookstore/urls/storage.py
@@ -3,6 +3,7 @@ from django.urls import path
 from ..views.storage import AddBookToStorageViewAPI, GetBookStorageViewWithIdAPI, GetBookStorageViewAPI, GetMonthReportViewAPI
 
 urlpatterns = [
+    path('', GetBookStorageViewAPI.as_view(), name='get-book-storage'),
     path('reports', GetMonthReportViewAPI.as_view(), name='get-book-storage-report'),
     path('<int:id>', GetBookStorageViewWithIdAPI.as_view(), name='get-book-storage-with-id'),
     path('book/new', AddBookToStorageViewAPI.as_view(), name='add-book-to-storage'),

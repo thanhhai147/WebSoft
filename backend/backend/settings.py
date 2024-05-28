8375551ad73d69be37e7cbed60f4c124e204da9c
Minh Nguyen <anhminh.dev@gmail.com>
2024-05-28 11:20:24 +0700

    Update static file configuration for production deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,8 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
+# The absolute path to the directory where static files will be collected.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

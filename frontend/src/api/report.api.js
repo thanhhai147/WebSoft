72b0050eb9add0338523ff6fc24ee0b8ae92ff78
thanhhai252004 <thanhhai252004@gmail.com>
2024-05-29 21:00:10 +0700

    Add debt report feature

--- frontend/src/api/report.api.js
+++ frontend/src/api/report.api.js
@@ -4,6 +4,10 @@ const ReportAPI = {
     getStorageReport: (dateData) => {
         return BaseAPI.post("/storage/reports", dateData)
     },
+
+    getDebtReport: (dateData) => {
+        return BaseAPI.post("/consumer/debtReport", dateData)
+    }
 }
 
 export default ReportAPI

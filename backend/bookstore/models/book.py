90e8c77c80bda0b8fe9634598d0608c6d220587a
thanhhai252004 <thanhhai252004@gmail.com>
2024-06-05 21:29:15 +0700

    Modify database tables's constraints to avoid overlap

--- backend/bookstore/models/book.py
+++ backend/bookstore/models/book.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator
 
 class BookType(models.Model):
     BookTypeId = models.AutoField(primary_key=True)
-    BookTypeName = models.TextField(max_length=255, null=False, blank=False)
+    BookTypeName = models.TextField(null=False, blank=False)
     Created = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
@@ -12,7 +12,7 @@ class BookType(models.Model):
     
 class Author(models.Model):
     AuthorId = models.AutoField(primary_key=True)
-    AuthorName = models.TextField(max_length=255, null=False, blank=False)
+    AuthorName = models.TextField(null=False, blank=False)
     Created = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
@@ -20,7 +20,7 @@ class Author(models.Model):
     
 class Book(models.Model):
     BookId = models.AutoField(primary_key=True)
-    BookName = models.TextField(max_length=255, null=False, blank=False)
+    BookName = models.TextField(null=False, blank=False)
     BookTypeId = models.ForeignKey(BookType, on_delete=models.SET_NULL, null=True, blank=False)
     AuthorId = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=False)
     Quantity = models.IntegerField(default=0, null=False, blank=False, validators=[MinValueValidator(0)])
--- backend/bookstore/models/consumer.py
+++ backend/bookstore/models/consumer.py
@@ -5,10 +5,10 @@ from ..validators.models_validator import ConsumerValidator
 
 class Consumer(models.Model):
     ConsumerId = models.AutoField(primary_key=True)
-    Name = models.TextField(max_length=255, null=False, blank=False)
-    Address = models.TextField(max_length=255, null=False, blank=False)
-    Phone = models.TextField(max_length=11, null=False, blank=False, validators=[ConsumerValidator.Phone])
-    Email = models.EmailField(max_length=320, null=True, blank=True)
+    Name = models.TextField(null=False, blank=False)
+    Address = models.TextField(null=False, blank=False)
+    Phone = models.TextField(null=False, blank=False, validators=[ConsumerValidator.Phone])
+    Email = models.EmailField(null=True, blank=True)
     Debt = models.FloatField(default=0, null=False, blank=False, validators=[MinValueValidator(0)])
     Created = models.DateTimeField(default=timezone.now)
     
